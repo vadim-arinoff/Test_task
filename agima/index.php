45d92ba05f913812f2fa7da8c6eae941bb246d2e
Vadya <74736081+vadim-arinoff@users.noreply.github.com>
2025-02-24 17:51:12 +0300

    Update index.php

--- agima/index.php
+++ agima/index.php
@@ -18,3 +18,5 @@
         <input type="submit" value="Отправить">
     </div>
     </form>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="form-handler.js"></script>
